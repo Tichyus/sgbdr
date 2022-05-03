a361378190c9d4b437598b02c3da4879f71dde84
tichyus <quq.accoun@gmail.com>
2022-05-03 03:29:41 +0200

    add items to test

--- resources/js/frontend/components/TableBoot.vue
+++ resources/js/frontend/components/TableBoot.vue
@@ -9,7 +9,27 @@ import { mapState, mapMutations, mapActions } from 'vuex';
 
 export default {
     name: "TableBoot",
-    
+    data() {
+        return {
+            items: [
+                { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+                { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+                {
+                    age: 89,
+                    first_name: 'Geneva',
+                    last_name: 'Wilson',
+                    _rowVariant: 'danger'
+                },
+                {
+                    age: 40,
+                    first_name: 'Thor',
+                    last_name: 'MacDonald',
+                    _cellVariants: { age: 'info', first_name: 'warning' }
+                },
+                { age: 29, first_name: 'Dick', last_name: 'Dunlap' }
+            ]
+        }
+    }
 }
 </script>
 
