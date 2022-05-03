e8d8caa3a4b552b6b7cc9efb4db7e3d03086a2dc
tichyus <quq.accoun@gmail.com>
2022-05-03 18:07:29 +0200

    test

--- resources/js/frontend/components/TableBoot.vue
+++ resources/js/frontend/components/TableBoot.vue
@@ -12,7 +12,7 @@ export default {
     data() {
         return {
             items: [
-                { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+                { age: 42, first_name: 'Dickerson', last_name: 'Macdonald' },
                 { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
                 {
                     age: 89,
